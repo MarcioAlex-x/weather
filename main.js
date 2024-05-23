f9d448a6f93af44c07e4e02f0337f88c8549f0cb
MarcioAlex-x <marcioalex586@gmail.com>
2024-05-22 23:13:51 -0300

    Implementação do loading

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ window.addEventListener('load',()=>{
     navigator.geolocation.getCurrentPosition((position)=>{
         const latitude = position.coords.latitude
         const longitude = position.coords.longitude
+        document.querySelector('.main').classList.add('hide')
+        document.querySelector('.waitingReponse').innerHTML = '<h2>Esperando resposta</h2>'
 
         const getData = async()=>{
+            
             const fetchData = await fetch(`https://api.weatherapi.com/v1/current.json?key=64930df30e9240b6be3214240242205&q=${latitude},${longitude}&lang=pt`)
             const data = await fetchData.json()
+            document.querySelector('.main').classList.remove('hide')    
+            document.querySelector('.waitingReponse').innerHTML = ''        
 
             if(data.current.is_day === 0){
                 document.querySelector('.container').classList.add('night')
